70248a7471e06fa784dad3e52cc493880efb8785
abinashsinha17 <67058632+abinashsinha17@users.noreply.github.com>
2026-06-18 16:31:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 # Store your API keys here
 
-LLAMA_API_KEY = "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"
-NEMOTRON_API_KEY = "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"
+LLAMA_API_KEY = "nvapi-"
+NEMOTRON_API_KEY = "nvapi-"
